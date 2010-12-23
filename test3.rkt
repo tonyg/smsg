e955eb96c0330358bfee3e64e64362663b458b14
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2010-12-22 22:35:31 -0500

    Update test3

--- test3.rkt
+++ test3.rkt
@@ -8,6 +8,9 @@
 (define k (half-queue 'k))
 
 (client (list 'server 'test3 "localhost" 5671))
+(post! 'server 'factory `(create! queue q1 test3 k))
+(write (k)) (newline)
+
 (do ((i 0 (+ i 1)))
     ((= i 1000000))
   (post! 'server 'q1 `(post! #f ,i #f)))
